71064266e667552c462ca5894465a6c1213dd8a8
WorstDevEver <tyl3r684@gmail.com>
2015-07-07 01:20:11 -0400

    Revert "ok good"
    This reverts commit cd00d812468095fe81eb3b2878313bc8adfcaf29.

--- config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key:         '1Gmhe1dKfrz1bpuXqdD1gQKSb',
-  consumer_secret:      'Kd0Ght7Ysmy7QNQ8nldqGFGLNHEVVMZX8yZ49981Nu3ghR34KF',
-  access_token:         '2324970013-q3chu0jia0M3nQcgEflRqWBMOgIBvbe1gKuUoDK',
-  access_token_secret:  'vmV8tHCNCRjDIfpyX5e1wv6GYKXQj9xFdvcnXs2rz4zh0'
-}
